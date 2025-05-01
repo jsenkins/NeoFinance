b3e1b6b0f53f8e8a186c9d376ff1b1bbc0fa9eb7
Javeria Shahid <133033153+jsenkins@users.noreply.github.com>
2025-05-01 15:10:27 +0500

    Update App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,14 @@
+        
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./assets/pages/login";
-import Register from "./assets/pages/register";
-import Analytics from "./assets/pages/analytics";
-import Profile from "./assets/pages/profile";
+
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Analytics from "./pages/analytics";
+import Profile from "./pages/profile";
+import Bills from "./pages/Bills";
+import DebtData from "./pages/budgeting";
+import Budgeting from "./pages/budgeting";
+
 
 function App() {
   return (
@@ -10,12 +16,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        
+
+
         <Route path="/analytics" element={<Analytics />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/bills" element={<Bills />} />
+        <Route path="/debtdata" element={<DebtData />} />
+        <Route path="/budgeting" element={<Budgeting />} />
 
         
+
+
 
 
 
